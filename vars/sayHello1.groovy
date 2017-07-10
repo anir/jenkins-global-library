963bf967f95a3e85f82cf9360eed948832b3aad8
Anirban Das <anirban-bimal.das@kp.org>
2017-07-09 20:44:38 -0700

    modifying package defination

--- vars/sayHello1.groovy
+++ vars/sayHello1.groovy
@@ -7,8 +7,8 @@ def call(body) {
  body.resolveStrategy = Closure.DELEGATE_FIRST
  body.delegate = config
  body()
- // def z = new org.foo.TestCheckout()
- // z.checkOutFrom("myrepo")
+ def z = new org.foo.TestCheckout()
+ z.checkOutFrom("myrepo")
  echo "Hello World from " + "${config.name}"
 
 
@@ -20,11 +20,11 @@ println()
 println("Total nodes: [" + Jenkins.instance.computers.size() + "]")
 println("Total executors: [" + Jenkins.instance.computers.inject(0, {a, c -> a + c.numExecutors}) + "]")
 
-
+/*
 File file = new File("newfile.txt");
 
 if(file.createNewFile()){
   println("creating new file.....")
 }
-
+*/
 }
