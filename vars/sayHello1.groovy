c2a76cfa8e13635c2002f97e9d49ff07b234124a
Anirban Das <anirban-bimal.das@kp.org>
2017-07-09 18:52:07 -0700

    using functions under src

--- vars/sayHello1.groovy
+++ vars/sayHello1.groovy
@@ -1,4 +1,4 @@
-import org.foo.TestCheckout
+import jenkins.model.Jenkins
 
 def call(body) {
 // evaluate the body block, and collect configuration into the object
@@ -6,7 +6,16 @@ def call(body) {
  body.resolveStrategy = Closure.DELEGATE_FIRST
  body.delegate = config
  body()
- def z = new org.foo.TestCheckout()
- z.checkOutFrom("myrepo")
+ // def z = new org.foo.TestCheckout()
+ // z.checkOutFrom("myrepo")
  echo "Hello World from " + "${config.name}"
+
+
+
+println("All Nodes - executor count:")
+println()
+Jenkins.instance.computers.eachWithIndex() { c, i -> println " [${i+1}] ${c.displayName}: ${c.numExecutors}" };
+println()
+println("Total nodes: [" + Jenkins.instance.computers.size() + "]")
+println("Total executors: [" + Jenkins.instance.computers.inject(0, {a, c -> a + c.numExecutors}) + "]")
 }
