840db51b54453d97f7974764cd7cbfb445329979
Anirban Das <anirban-bimal.das@kp.org>
2017-07-10 00:36:16 -0700

    modifying package defination

--- src/org/foo/TestChekout.groovy
+++ src/org/foo/TestChekout.groovy
@@ -7,8 +7,19 @@ import hudson.EnvVars
 import hudson.FilePath
 
     def downloadFile(filepath){
-        File f = new File("${filepath}\newtest.txt")
-        f.createNewFile()
+    try {
+
+    File file = new File("newfile.txt");
+
+    if (file.createNewFile()){
+      System.out.println("File is created!");
+    }else{
+      System.out.println("File already exists.");
+    }
+
+  } catch (IOException e) {
+    e.printStackTrace();
+}
     }
 
 
